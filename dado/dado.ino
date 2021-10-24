5e574851b4b384cd39bb839767f7e8de5e8f7bd3
Projetuino <projetuino@outlook.com>
2021-10-24 10:56:59 -0300

    First commit

---
+++ dado/dado.ino
@@ -0,0 +1,50 @@
+//Mapeamento dos pinos do display na
+//sequencia {a, b, c, d, e, f, g, DP}
+const int pinos[8] = {5, 4, 9, 10, 11, 6, 7, 8};
+const int numeros[10][8] = {
+  {1, 1, 1, 1, 1, 1, 0, 0},
+  {0, 1, 1, 0, 0, 0, 0, 0},
+  {1, 1, 0, 1, 1, 0, 1, 0},
+  {1, 1, 1, 1, 0, 0, 1, 0},
+  {0, 1, 1, 0, 0, 1, 1, 0},
+  {1, 0, 1, 1, 0, 1, 1, 0},
+  {1, 0, 1, 1, 1, 1, 1, 0},
+  {1, 1, 1, 0, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1, 0},
+  {1, 1, 1, 1, 0, 1, 1, 0}
+};
+const int botao = 2;
+int numero = 0;
+
+void setup() {
+  for (int i = 0; i < 8; i++) {
+    pinMode(pinos[i], OUTPUT);
+  }
+  pinMode(botao, INPUT);
+  randomSeed(A0);
+}
+
+void loop() {
+  if (digitalRead(botao) == 1) {
+    numero = random(1, 7);
+    roleta();
+  }
+  mostrarNumero(numero);
+}
+
+void roleta() {
+  digitalWrite(pinos[6], LOW);
+  for (int i = 0; i <= 4; i++) {
+    for (int s = 0; s <= 5; s++) {
+      digitalWrite(pinos[s], HIGH);
+      delay(25);
+      digitalWrite(pinos[s], LOW);
+    }
+  }
+}
+
+void mostrarNumero(int num) {
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(pinos[i], numeros[num][i]);
+  }
+}
